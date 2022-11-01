5f386134cdbbdef577c960d9ad0071d3a4005278
zotovaad <115518296+zotovaad@users.noreply.github.com>
2022-11-01 13:51:02 +0300

    Add files via upload

--- Лабораторная работа 4/task_2.py
+++ Лабораторная работа 4/task_2.py
@@ -2,8 +2,8 @@ def get_count_char(str_):
     # TODO посчитать количество каждой буквы в строке в аргументе str_
     str_ = str_.lower()
     all_letters = []
-    for letter in str_:     # цикл для записи всех букв в строку, исключая знаки препинания
-        if letter not in ['.', ',', '!', '']:
+    for letter in str_:     # цикл для записи только букв в строку
+        if letter.isalpha():
             all_letters += letter.strip()
     dict_letters = {}   # словарь с подсчетом количества символов
     for letter in all_letters:
@@ -14,9 +14,18 @@ def get_count_char(str_):
     return dict_letters
 
 
+def get_ratio(dict_):    # функция, которая возвращает словарь с процентным содержанием букв
+    total_amount = sum(dict_.values())
+    for letter, amount in dict_.items():
+        ratio = round(amount / total_amount * 100, 2)
+        dict_[letter] = ratio
+    return dict_
+
+
 main_str = """
     Данное предложение будет разбиваться на отдельные слова. 
     В качестве разделителя для встроенного метода split будет выбран символ пробела. На выходе мы получим список отдельных слов. 
     Далее нужно отсортировать слова в алфавитном порядке, а после сортировки склеить их с помощью метода строк join. Приступим!!!!
 """
 print(get_count_char(main_str))
+
